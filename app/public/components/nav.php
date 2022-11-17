99a6901623ebbf8e5dca2d6d5133217323ded854
DevSoft <devonvanw@gmail.com>
2022-11-17 08:30:59 +0100

    Signup

---
+++ app/public/components/nav.php
@@ -0,0 +1,18 @@
+<nav class="fixed top-0 w-full z-50" style="backdrop-filter: blur(20px);">
+    <div class="flex flex-row justify-between px-4 py-6">
+        <h1 class="text-teal-800 font-black uppercase text-4xl leading-tight tracking-tight">Tosti</h1>
+        <div><button
+                class="bg-teal-800 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                <span
+                    class="text-teal-800 group-hover:text-white uppercase font-bold relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-teal-800">
+                    Login
+                </span>
+            </button><button
+                class="bg-teal-800 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                <span
+                    class="text-white group-hover:text-teal-800 uppercase font-bold relative px-5 py-2.5 transition-all ease-in duration-75 bg-teal-800 rounded-md group-hover:bg-white">
+                    Signup
+                </span>
+            </button></div>
+    </div>
+</nav>
--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,36 @@
-<?php
-echo "Requested URL: " . $_SERVER['REQUEST_URI'];
-phpinfo();
+<script src="https://cdn.tailwindcss.com"></script>
+<html>
+<?php $feed = [(object)["title" => "Tosti", "img" => "https://soulfood.nl/wp-content/uploads/2021/01/tosti.jpg", "desc" => "Dit is een test.", "likes" => 12, "account" => (object)["id" => 1, "name" => "Bert"]], (object)["title" => "Broodje", "img" => "https://soulfood.nl/wp-content/uploads/2021/01/tosti.jpg", "desc" => "Dit is een test.", "likes" => 12, "account" => (object)["id" => 1, "name" => "Bert"]]] ?>
+
+<body>
+    <div class="min-h-screen">
+        <?php include 'components/nav.php' ?>
+        <div class="flex justify-center mt-32">
+            <div class="max-w-xl">
+                <?php foreach($feed as $item) : ?>
+                <div class="bg-gray-100 rounded-lg overflow-hidden relative mb-12">
+                    <div class="absolute top-0 left-0 w-full p-2 pb-4 bg-gradient-to-b from-gray-800 to-transparent">
+                        <h2 class="text-white font-extrabold text-2xl"><?= $item->account->name ?></h2>
+                    </div>
+                    <img src="<?= $item->img ?>">
+                    </img>
+                    <div class="p-4 relative">
+                        <div class="flex items-center justify-center absolute top-2 right-2">
+                            <p class="text-teal-800"><?= $item->likes ?></p><svg xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                class="w-6 h-6 text-teal-800 hover:fill-teal-800 cursor-pointer">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                            </svg>
+                        </div>
+                        <h2 class="text-teal-800 font-bold text-xl"><?= $item->title ?></h2>
+                        <p><?= $item->desc ?></p>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
